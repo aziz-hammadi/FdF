bc7de9d6379353b51506109f1b023a9d286a8a3d
aziz-hammadi <ahammad@student.42.fr>
2021-10-13 23:38:41 +0200

    normer

--- libft/get_next_line.c
+++ libft/get_next_line.c
@@ -3,16 +3,22 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mnaji <mnaji@student.42.fr>                +#+  +:+       +#+        */
+/*   By: ahammad <ahammad@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/28 16:48:07 by mnaji             #+#    #+#             */
-/*   Updated: 2019/04/29 11:41:10 by mnaji            ###   ########.fr       */
+/*   Created: 2019/02/28 16:48:07 by ahammad           #+#    #+#             */
+/*   Updated: 2021/10/13 23:38:25 by ahammad          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static int			strchr_line(char **stock, char **line)
+int	ptr_assign2(int *variable, int valeur)
+{
+	*variable = valeur;
+	return (*variable);
+}
+
+static int	strchr_line(char **stock, char **line)
 {
 	char			*tmp_stock;
 	int				i;
@@ -23,27 +29,45 @@ static int			strchr_line(char **stock, char **line)
 		if (tmp_stock[i++] == '\0')
 			return (0);
 	tmp_stock[i] = '\0';
-	if (!(*line = ft_strdup(*stock)))
+	*line = ft_strdup(*stock);
+	if (!*line)
 		return (-1);
-	if (!(*stock = ft_strdup_free(*stock, &tmp_stock[i] + 1)))
+	*stock = ft_strdup_free(*stock, &tmp_stock[i] + 1);
+	if (!*stock)
 		return (-1);
 	return (1);
 }
 
-static	int			read_file(int fd, char **stock, char **line)
+int	ft_init_stock(char *buff, char **stock)
+{
+	if (*stock)
+	{
+		*stock = ft_strjoin_free(*stock, *stock, buff);
+		if (!*stock)
+			return (-1);
+	}
+	if (!*stock)
+	{			
+		*stock = ft_strdup(buff);
+		if (!*stock)
+			return (-1);
+	}
+	return (0);
+}
+
+static int	read_file(int fd, char **stock, char **line)
 {
 	int				ret;
 	char			buff[BUFF_SIZE + 1];
 	int				eol;
 
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	while ((ptr_assign2(&ret, read(fd, buff, BUFF_SIZE))) > 0)
 	{
 		buff[ret] = '\0';
-		if (*stock && !(*stock = ft_strjoin_free(*stock, *stock, buff)))
+		if (ft_init_stock(buff, stock) == -1)
 			return (-1);
-		if (!*stock && !(*stock = ft_strdup(buff)))
-			return (-1);
-		if ((eol = strchr_line(stock, line)) == 1)
+		eol = strchr_line(stock, line);
+		if (eol == 1)
 			break ;
 		if (eol == -1)
 			return (-1);
@@ -53,17 +77,22 @@ static	int			read_file(int fd, char **stock, char **line)
 	return (1);
 }
 
-int					get_next_line(int const fd, char **line)
+int	get_next_line(int const fd, char **line)
 {
 	static char		*stock[OPEN_MAX];
 	int				eof;
 
-	if (!line || fd < 0 || fd > OPEN_MAX || (read(fd, stock[fd], 0) != 0)\
+	if (!line || fd < 0 || fd > OPEN_MAX || (read(fd, stock[fd], 0) != 0)
 		|| BUFF_SIZE <= 0)
 		return (-1);
 	if (stock[fd])
-		if ((eof = strchr_line(&stock[fd], line)) != 0)
-			return ((eof == -1) ? -1 : 1);
+	{
+		eof = strchr_line(&stock[fd], line);
+		if (eof == -1)
+			return (-1);
+		else if (eof != 0)
+			return (1);
+	}
 	eof = read_file(fd, &stock[fd], line);
 	if (eof != 0 || stock[fd] == NULL || stock[fd][0] == '\0')
 		return (eof);
